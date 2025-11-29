a0775c8d00f4c79f7c404b0c9150605e5c84e170
blackasusa <25005844@siswa.um.edu.my>
2025-11-29 20:36:52 +0800

    idk2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/sdghackathon-1/",
+    base: "/sdghackathon-1",
 });
